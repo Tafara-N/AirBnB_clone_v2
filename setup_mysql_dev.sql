3fc988eb55bd2179de7a1154c286c1f1deaac3a9
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-18 18:46:56 +0200

    A script that prepares a MySQL server for the project

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -4,7 +4,7 @@
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 -- Create user if it doesn't already exist
-CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_db';
 
 -- Grant all privileges on the hbnb_dev_db database to the hbnb_dev user
 GRANT ALL PRIVILEGES ON hbnb_dev_db . * TO 'hbnb_dev'@'localhost';
