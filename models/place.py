a7723672c171ae9b1feb7346930d2372e6c16d83
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-19 18:30:22 +0200

    Place class

--- models/place.py
+++ models/place.py
@@ -42,6 +42,7 @@ class Place(BaseModel, Base):
     """
 
     __tablename__ = "places"
+
     city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
     user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
     name = Column(String(128), nullable=False)
