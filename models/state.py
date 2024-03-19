a75b0b151f9edf9490ee7beb7b02a256ac9be9e5
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-19 09:14:25 +0200

    Modified State class

--- models/state.py
+++ models/state.py
@@ -1,8 +1,42 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+
+"""
+The State class
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
+import models
+from models.city import City
+import shlex
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """
+    State class
+
+    Attributes:
+        name: input name
+    """
+
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
+
+    @property
+    def cities(self):
+        var = models.storage.all()
+        our_list = []
+        result = []
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                our_list.append(var[key])
+        for element in our_list:
+            if (element.state_id == self.id):
+                result.append(element)
+        return (result)
