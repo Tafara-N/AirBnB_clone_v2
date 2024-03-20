f37eac205cec60ddcd5bbd582e9b05dcf3c4d4c6
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-20 11:52:47 +0200

    A class to manage file storage for hbnb clone.

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -45,8 +45,16 @@ class FileStorage:
             return self.__objects
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        """
+        Adds new object to storage dictionary
+
+        Args:
+            obj: Object
+        """
+
+        if obj:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
         """Saves storage dictionary to file"""
