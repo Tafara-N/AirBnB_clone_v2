96fb00da43f2b28216d805b870453c03bf87c8bc
Brendon45 <jejebrendon722@gmail.com>
2024-03-20 01:15:18 -0700

    fixed pycodestyle

--- models/state.py
+++ models/state.py
@@ -10,6 +10,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 from models.city import City
 
+
 class State(BaseModel, Base):
     """
     State class
@@ -20,22 +21,24 @@ class State(BaseModel, Base):
 
     __tablename__ = "states"
     if storage_type == 'db':
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', backref='state',
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
                               cascade='all, delete, delete-orphan')
+
+
 else:
     name = ''
 
     @property
     def cities(self):
-            '''Returns the list of City instances with state_id
+        '''Returns the list of City instances with state_id
                 equals the current State.id
                 Returns FileStorage relationship between State and City
-            '''
-            from models import storage
-            related_cities = []
-            cities = storage.all(City)
-            for city in cities.values():
-                if city.state_id == self.id:
-                    related_cities.append(city)
-            return related_cities
+        '''
+        from models import storage
+        related_cities = []
+        cities = storage.all(City)
+        for city in cities.values():
+            if city.state_id == self.id:
+                related_cities.append(city)
+        return related_cities
