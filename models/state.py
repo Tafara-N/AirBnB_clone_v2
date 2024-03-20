6bbf094805d2a2e568a334638ba3a589d0721ea9
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-20 10:52:55 +0200

    Modified State class

--- models/state.py
+++ models/state.py
@@ -4,11 +4,13 @@
 The State class for the HBNB project
 """
 
-from models import storage_type
+from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
-from sqlalchemy import Column, String
+from sqlalchemy import Column, Integer, String
+import models
 from models.city import City
+import shlex
 
 
 class State(BaseModel, Base):
@@ -16,27 +18,32 @@ class State(BaseModel, Base):
     State class
 
     Attributes:
-        name: input name
+        name: Input name
     """
 
     __tablename__ = "states"
-    if storage_type == 'db':
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state',
-                              cascade='all, delete, delete-orphan')
-    else:
-        name = ''
+
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", cascade='all, delete, delete-orphan',
+                          backref="state")
 
     @property
     def cities(self):
-        '''Returns the list of City instances with state_id
+        '''
+        Returns the list of City instances with state_id
                 equals the current State.id
                 Returns FileStorage relationship between State and City
         '''
-        from models import storage
+
+        var = models.storage.all()
+        our_list = []
         related_cities = []
-        cities = storage.all(City)
-        for city in cities.values():
-            if city.state_id == self.id:
-                related_cities.append(city)
-        return related_cities
+        for key in var:
+            city = key.replace('.', ' ')
+            city = shlex.split(city)
+            if (city[0] == 'City'):
+                our_list.append(var[key])
+        for element in our_list:
+            if (element.state_id == self.id):
+                related_cities.append(element)
+        return (related_cities)
