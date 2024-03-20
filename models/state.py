e8f7f5e71c972b8e8a2ab4d8ce373b20046c1240
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-03-20 10:37:09 +0200

    Modified State class

--- models/state.py
+++ models/state.py
@@ -24,10 +24,8 @@ class State(BaseModel, Base):
         name = Column(String(128), nullable=False)
         cities = relationship('City', backref='state',
                               cascade='all, delete, delete-orphan')
-
-
-else:
-    name = ''
+    else:
+        name = ''
 
     @property
     def cities(self):
