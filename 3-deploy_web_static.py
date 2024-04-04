c05da92a536740fed25326b3df5cc0340d2e59a2
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-04-04 20:21:17 +0200

    Distrubiting archives on the web servers based on the `2-do_deploy_web-static` file.

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -24,9 +24,9 @@ def do_pack():
         date = datetime.now().strftime("%Y%m%d%H%M%S")
         if isdir("versions") is False:
             local("mkdir versions")
-        file_name = "versions/web_static_{}.tgz".format(date)
-        local("tar -cvzf {} web_static".format(file_name))
-        return file_name
+        file_nameame = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_nameame))
+        return file_nameame
     except:
         return None
 
@@ -43,13 +43,13 @@ def do_deploy(archive_path):
     if exists(archive_path) is False:
         return False
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
+        file_name = archive_path.split("/")[-1]
+        no_ext = file_name.split(".")[0]
         path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
         run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_ext))
+        run('rm /tmp/{}'.format(file_name))
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
         run('rm -rf {}{}/web_static'.format(path, no_ext))
         run('rm -rf /data/web_static/current')
