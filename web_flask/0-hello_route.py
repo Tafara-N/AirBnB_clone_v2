68c9f17114df23d7ab36ced4a3da5360e6be614d
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-04-19 20:34:11 +0200

    A script that starts a Flask web application.

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,6 +9,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_route():
     """
@@ -17,6 +18,7 @@ def hello_route():
     """
 
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
