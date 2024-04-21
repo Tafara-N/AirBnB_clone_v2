f0ed66e6ee63ce3db0b5206567d977257e0ee7d9
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-04-21 23:35:08 +0200

    A script that starts a Flask web application.

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+/hbnb_filters: Displays an HTML page like 6-index.html
+    :State, City and Amenity objects loaded from DBStorage and
+    sorted by name (A->Z)
+"""
+
+from flask import Flask, render_template
+from models import *
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def filters():
+    """
+    Displays an HTML page from 'static'
+    """
+    states = storage.all("State").values()
+    amenities = storage.all("Amenity").values()
+    return render_template("10-hbnb_filters.html", states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    Closes the DB_Storage on teardown
+    """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
