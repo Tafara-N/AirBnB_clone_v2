53b5484a6224685ee8ecd263515a19dc39f5189f
Tafara-N <c.tafaranyamhunga@gmail.com>
2024-04-23 12:17:12 +0200

    A script that prepares a MySQL server for the project.

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -11,5 +11,5 @@ GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
 
 -- Grant SELECT privilege on the performance_schema database to the hbnb_test user
 GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
-SET GLOBAL validate_password.policy=LOW;
+SET GLOBAL validate_password.policy=MEDIUM;
 FLUSH PRIVILEGES;
